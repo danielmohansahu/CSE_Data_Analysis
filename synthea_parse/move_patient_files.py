1806d523f6a2919babfbe7153f2fcd793a5dd050
Daniel Sahu <danielmohansahu@gmail.com>
2016-11-16 11:35:20 -0500

    Updated patient data

--- synthea_parse/move_patient_files.py
+++ synthea_parse/move_patient_files.py
@@ -1,6 +1,7 @@
 import synthea_parse_lib as spb
 from shutil import copy
 
+print("Collecting patient files...\n")
 patient_files = spb.get_patients("../../Patient_Records_New/Output/fhir")
 
 count = 0
@@ -11,14 +12,12 @@ for patient in patient_files:
 	if spb.has_condition(patient,'heart disease'):
 		hd = hd + 1
 		copy(patient,"../Patient_Data/Heart_Disease/")
-	elif nhd < 1000:
+	elif nhd < 2116:
 		try:
 			len(spb.get_biometrics_gen(patient).keys())		# To see if the function worked
 			copy(patient,"../Patient_Data/Healthy/")
 			nhd = nhd + 1
 		except:
 			print "Skipping a healthy patient! Bias? ..."
-	print "Patient #%d/%d " %(count,len(patient_files))
+	print "Patient #%d/%d; %d with heart disease" %(count,len(patient_files),hd)
 
-
-print "Total of %d / %d people with heart disease" %(hd,count)
