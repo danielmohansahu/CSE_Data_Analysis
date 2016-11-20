e52a39f52dbb489c65f43dd81e9ccb83d728dccb
lshen28 <lshen28@its.jnj.com>
2016-11-19 19:00:39 -0500

    Additional Chanages made to RandomForestMethods, using ROC as measurements, tunning still in progress, aim to achieve 80%, boosting seems to perform worse than RF

--- RandomForestMethod.R
+++ RandomForestMethod.R
@@ -17,7 +17,7 @@ metric <- "Accuracy"
 set.seed(seed)
 mtry <- sqrt(ncol(x))
 tunegrid <- expand.grid(.mtry=mtry)
-rf_default <- train(x, y, method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
+rf_default <- train(x, y, method="rf", tuneGrid=tunegrid, trControl=control)
 print(rf_default)
 
 #testing
@@ -32,6 +32,36 @@ plot(rf_random)
 
 #Grid Search
 control <- trainControl(method="repeatedcv", number=10, repeats=3, search="grid")
+set.seed(seed)
+tunegrid <- expand.grid(.mtry=c(1:25))
+rf_gridsearch <- train(x, y, method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
+print(rf_gridsearch)
+plot(rf_gridsearch)
+
+#-----------------------------------------------------------------------------------------------------------
+#ROC
+# Create model with default paramters
+control <- trainControl(method="repeatedcv", number=10, repeats=3, classProbs = TRUE, summaryFunction = twoClassSummary)
+seed <- 7
+metric <- "ROC"
+set.seed(seed)
+mtry <- sqrt(ncol(x))
+tunegrid <- expand.grid(.mtry=mtry)
+rf_default <- train(x, y, method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
+print(rf_default)
+
+#testing
+
+# Random Search
+control <- trainControl(method="repeatedcv", number=10, repeats=3, search="random", classProbs = TRUE, summaryFunction = twoClassSummary)
+set.seed(seed)
+mtry <- sqrt(ncol(x))
+rf_random <- train(x, y, method="rf", metric=metric, tuneLength=25, trControl=control)
+print(rf_random)
+plot(rf_random)
+
+#Grid Search
+control <- trainControl(method="repeatedcv", number=10, repeats=3, search="grid",classProbs = TRUE, summaryFunction = twoClassSummary)
 set.seed(seed)
 tunegrid <- expand.grid(.mtry=c(1:25))
 rf_gridsearch <- train(x, y, method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
--- boostingMethod.R
+++ boostingMethod.R
@@ -2,11 +2,16 @@ library(randomForest)
 library(mlbench)
 library(caret)
 library(gbm)
+library(dplyr)
 
-trainData = read.csv("D:/Github/CSE_Data_Analysis/synthea_data_train_R.csv",header=TRUE)
-testData = read.csv("D:/Github/CSE_Data_Analysis/synthea_data_test_R.csv",header=TRUE)
+trainData = read.csv("D:/Github/CSE_Data_Analysis/CSE_Data_Analysis/synthea_data_train.csv",header=TRUE)
+testData = read.csv("D:/Github/CSE_Data_Analysis/CSE_Data_Analysis/synthea_data_test.csv", header = TRUE)
+x = trainData[,1:24]
+y = trainData[,25]
+summary(trainData)
 
-fit = gbm(Heart.Disease~., data=trainData, distribution="gaussian")
+
+fit = gbm.fit(x, y, distribution="bernoulli", n.trees = 7000, shrinkage = 0.001, interaction.depth = 3, n.minobsinnode = 10)
 
 # summarize the fit
 summary(fit)
