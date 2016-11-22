51363338b67a3f17e4768181616f91584370edc7
Daniel Sahu <danielmohansahu@gmail.com>
2016-11-22 13:12:11 -0500

    updated visualization w/ user path

--- synthea_parse/test_playground.py
+++ synthea_parse/test_playground.py
@@ -1,8 +1,34 @@
 import synthea_parse_lib as spb
 from shutil import copy
 
-patient = "diabetic.json"
+print("Collecting patient files...\n")
+patient_files = spb.get_patients("../../Patient_Records_New/Output/fhir")
 
-vals = spb.get_biometrics_gen(patient)
+count = 0
+hd1 = 0
+hd2 = 0
+hd3 = 0
+hd4 = 0
+for patient in patient_files:
+	count = count + 1
+	if spb.get_keyword(patient,"53741008"):
+		hd1 = hd1 + 1;
+		print "%d patients with 53741008" %(hd1)
+	elif spb.get_keyword(patient,"72092001"):
+		hd2 = hd2 + 1;
+		print "%d patients with 72092001" %(hd2)
+	elif spb.get_keyword(patient,"414024009"):
+		hd3 = hd3 + 1;
+		print "%d patients with 414024009" %(hd3)
+	elif spb.get_keyword(patient,"128599005"):
+		hd4 = hd4 + 1;
+		print "%d patients with 128599005" %(hd4)
+	else:
+		pass
 
-print vals;
+print "----------------------------------------------"
+print "%d patients with 53741008" %(hd1)
+print "%d patients with 72092001" %(hd2)
+print "%d patients with 414024009" %(hd3)
+print "%d patients with 128599005" %(hd4)
+print "%d patients with heart disease." %(hd1 + hd2 + hd3 + hd4)
